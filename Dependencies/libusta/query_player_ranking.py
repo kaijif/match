dfce6568f6ff7135f42d24bf1b1a53353d29a74c
thyConquerer <53155815+thyConquerer@users.noreply.github.com>
2021-07-14 18:16:24 -0400

    Update query_player_ranking.py

--- Dependencies/libusta/query_player_ranking.py
+++ Dependencies/libusta/query_player_ranking.py
@@ -1,13 +1,13 @@
+
 import pandas as pd
+
 
 
 def query_usta_ranking(name, div):
     df = pd.read_csv(f'Dependencies/tables/{div}.csv', index_col='Rank')
     df['Name'] = df['Name'].str.upper()
-    out = [df[df['Name'] == name].index.tolist()[0]] if len(df[df['Name'] == name].index.tolist()) >= 2 else df[
-        df['Name'] == name].index.tolist() if df[df['Name'] == name].index.tolist() else ['NaN']
-    out.append(df[df['Name'] == name]['City, State'].tolist()[0]) if df[df['Name'] == name][
-        'City, State'].tolist() else out.append('NaN')
+    out = [df[df['Name'] == name].index.tolist()[0]] if len(df[df['Name'] == name].index.tolist()) >= 2 else df[df['Name'] == name].index.tolist() if df[df['Name'] == name].index.tolist() else ['NaN']
+    out.append(df[df['Name'] == name]['City, State'].tolist()[0]) if df[df['Name'] == name]['City, State'].tolist() else out.append('NaN')
     return out
 
 
