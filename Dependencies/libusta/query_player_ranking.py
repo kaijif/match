89b50cad05274db7c91f4f874adb688769894ed6
thyConquerer <53155815+thyConquerer@users.noreply.github.com>
2021-07-14 18:20:34 -0400

    Update query_player_ranking.py

--- Dependencies/libusta/query_player_ranking.py
+++ Dependencies/libusta/query_player_ranking.py
@@ -8,6 +8,8 @@ def query_usta_ranking(name, div):
     df['Name'] = df['Name'].str.upper()
     out = [df[df['Name'] == name].index.tolist()[0]] if len(df[df['Name'] == name].index.tolist()) >= 2 else df[df['Name'] == name].index.tolist() if df[df['Name'] == name].index.tolist() else ['NaN']
     out.append(df[df['Name'] == name]['City, State'].tolist()[0]) if df[df['Name'] == name]['City, State'].tolist() else out.append('NaN')
+    out.append(df[df['Name'] == name]['Section'].tolist()[0]) if df[df['Name'] == name][
+        'Section'].tolist() else out.append('NaN')
     return out
 
 
